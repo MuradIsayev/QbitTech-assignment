9963ab44b058f33dcafdc38991559822426e9c13
MurAd <muradisayevazer@gmail.com>
2023-11-17 00:13:38 +0400

    card view added for testing.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
+import house from '../assets/house.jpg';
 
 const Home = () => {
   return (
@@ -7,7 +8,33 @@ const Home = () => {
       <nav>
         <SearchBar />
       </nav>
-      Home
+      <div>
+        <h3 className="text-2xl mb-3">Houses</h3>
+        <div className="flex flex-wrap flex-row justify-between gap-2">
+          <div className="w-[320px] flex-grow-0 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img className="rounded-t-lg" src={house} alt="" />
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Noteworthy technology acquisitions 2021
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
