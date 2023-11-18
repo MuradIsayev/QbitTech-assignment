8f8c3276f08081223ee1d289950d386064b47530
MurAd <muradisayevazer@gmail.com>
2023-11-19 01:31:11 +0400

    Search component was updated

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,8 @@
-import { ModeToggle } from '../components/mode-toggle';
 import { Input } from '@/components/ui/input';
-import { Separator } from '@/components/ui/separator';
 
 const SearchBar = () => {
   return (
-    <nav className="w-full h-10 mb-6">
+    <nav className="w-full h-10 mt-5 mb-4">
       <div className="flex items-center justify-between gap-2 mb-3">
         <Input
           type="search"
@@ -13,9 +11,7 @@ const SearchBar = () => {
           placeholder="Search by address..."
           autoComplete="off"
         />
-        <ModeToggle />
       </div>
-      <Separator />
     </nav>
   );
 };
