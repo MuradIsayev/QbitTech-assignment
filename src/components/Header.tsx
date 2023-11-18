ac1da754d034a710a312f75c6582fb7053d4c6cd
MurAd <muradisayevazer@gmail.com>
2023-11-19 01:30:53 +0400

    Header component completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ModeToggle } from './mode-toggle';
+import { Separator } from './ui/separator';
+
+const Header = () => {
+  return (
+    <>
+      <nav className="flex items-center justify-between mb-2">
+        <h3 className="text-3xl">MyHouse</h3>
+        <ModeToggle />
+      </nav>
+      <Separator />
+
+    </>
+  );
+};
+
+export default Header;
