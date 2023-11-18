3acf1c67d199c537ac95e51cca2d81a33ba6a44a
MurAd <muradisayevazer@gmail.com>
2023-11-18 20:57:57 +0400

    Route added for single house page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import HouseDetails from './pages/HouseDetails';
 
 function App() {
   const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ function App() {
         <div className="main-container ">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/house/:id" element={<HouseDetails />} />
           </Routes>
         </div>
       </div>
