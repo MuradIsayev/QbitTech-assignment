e0f938320088135312ab563d29ef02c0610060be
MurAd <muradisayevazer@gmail.com>
2023-11-18 20:58:29 +0400

    House Details page was updated

--- src/pages/HouseDetails.tsx
+++ src/pages/HouseDetails.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { fetchHouseById } from '@/api/card';
+import { House } from '@/types/house';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+type HouseParams = {
+  id: string;
+};
 
 const HouseDetails = () => {
-  return <div>HouseDetails</div>;
+  const params = useParams<HouseParams>();
+  const { id } = params;
+
+  const parsedId = id ? parseInt(id, 10) : NaN;
+
+  const {
+    data: house,
+    isError,
+    isLoading
+  } = useQuery<House>({
+    queryKey: ['house', parsedId],
+    queryFn: () => fetchHouseById({ id: parsedId })
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError || !house) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return (
+    <div>
+      {house.address} - {parsedId}
+    </div>
+  );
 };
 
 export default HouseDetails;
