ba4ed3e89b8658634862571a9a6f58f0f14b4068
MurAd <muradisayevazer@gmail.com>
2023-11-19 01:31:01 +0400

    InfoBox component was created

---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,37 @@
+type ValueOfProperty = string | number | boolean;
+type UnitOfProperty = 'm²' | 'AZN';
+
+type Props = {
+  header: string;
+  icon?: string;
+  value: ValueOfProperty;
+  unit?: UnitOfProperty;
+};
+
+const InfoBox = ({ header, icon, value, unit }: Props) => {
+  if (value === true || value === false) value = value ? 'Yes' : 'No';
+
+  return (
+    <div
+      className={`flex flex-col ${
+        header === 'Description' ? 'grow' : 'w-48'
+      } w-48 p-2 bg-gray-200 rounded-lg h-28 dark:bg-gray-800`}
+    >
+      <h1 className="text-[1.2rem] uppercase font-semibold">{header}</h1>
+      <div className="flex items-center justify-center gap-2 mt-1">
+        <div className="object-contain w-6 h-6">
+          <img src={icon} alt="" />
+        </div>
+        <p
+          className={`text-[1.1rem] font-semibold text-gray-700 dark:text-gray-400 ${
+            header === 'Description' ? 'text-[.91rem]' : 'text-[1.1rem]'
+          }`}
+        >
+          {value} {unit}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBox;
