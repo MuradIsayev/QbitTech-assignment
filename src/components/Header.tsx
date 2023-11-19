1f3e816e734d8ebdbc2fbdc04fc238c1b5b1eaa0
MurAd <muradisayevazer@gmail.com>
2023-11-19 17:47:22 +0400

    CI/CD check

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <nav className="flex items-center justify-between mb-2">
-        <h3 className="text-3xl">MyHouse</h3>
+        <h3 className="text-3xl">MyHouse Deployed</h3>
         <ModeToggle />
       </nav>
       <Separator />
