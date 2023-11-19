b19ae18f5ba4e91cdbc45011fad85c7217c1f87d
MurAd <muradisayevazer@gmail.com>
2023-11-19 15:46:19 +0400

    Filters component was created

---
+++ src/components/Filters.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Input } from './ui/input';
+
+interface FiltersProps {
+  value: number | null;
+  name: string;
+  placeholder: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Filters = ({
+  value,
+  name,
+  placeholder,
+  handleInputChange
+}: FiltersProps) => {
+  return (
+    <>
+      <Input
+        type="number"
+        className={`max-w-[10rem] ${name === 'maxPrice' ? 'mr-6' : ''}`}
+        name={name}
+        value={value ?? ''}
+        onChange={handleInputChange}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
+
+export default Filters;
