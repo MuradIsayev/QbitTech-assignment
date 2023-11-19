79ea25bb3402a7da4bcedf07a9ac66022972c499
MurAd <muradisayevazer@gmail.com>
2023-11-19 17:04:44 +0400

    github deployment workflow was added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/qbitTech-assignment/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
